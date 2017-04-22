f3dfc3654303a35cad1ff7e66955ace76242cc75
Joseph Ramirez <jramirez98@cnm.edu>
2017-04-22 07:42:19 -0600

    added emailId to mySQL database and renamed index.php to email.php

--- sql/deveval.sql
+++ sql/deveval.sql
@@ -2,7 +2,8 @@ DROP TABLE if EXISTS email
 
 -- create the email entity
 CREATE TABLE email (
+emailId INT UNSIGHNED AUTO_INCREMENT NOT NULL,
 emailTimeSent TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
 emailAddressSent VARCHAR NOT NULL,
-PRIMARY KEY (email)
+PRIMARY KEY (emailId)
 );
